4d2bd61c5875ba20fe78c16313d31fb5ba44b910
jiazhiqiang <281460530@qq.com>
2018-11-15 17:44:33 +0800

    add buffer lesson

---
+++ seven/main.js
@@ -0,0 +1,19 @@
+'use strict';
+
+console.log('--------buffer study begin----------');
+
+// 创建
+
+// 写入
+
+// 读取
+
+//拷贝
+
+// 比较
+
+// 转化JSON
+
+
+
+console.log('--------buffer study end----------');
